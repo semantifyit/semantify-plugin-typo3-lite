5402428ed993c7b577daf34c511337a5347c66a0
Richard Dvorsky <generator@mydevelops.com>
2017-03-12 23:50:21 +0100

    NEW: added annotationID field to pages and their translations (language support)

---
+++ ext_tables.sql
@@ -0,0 +1,16 @@
+#
+# Modifying pages table
+#
+
+CREATE TABLE pages (
+
+  semantify_plugin_typo3_annotationID varchar(255) DEFAULT '' NOT NULL
+
+);
+
+#
+# Modifying pages_language_overlay table
+#
+CREATE TABLE pages_language_overlay (
+  semantify_plugin_typo3_annotationID varchar(255) DEFAULT '' NOT NULL
+);
