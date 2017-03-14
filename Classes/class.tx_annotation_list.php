69f6908171f72fb03647f31dbfaf5bb5b78bb902
Richard Dvorsky <generator@mydevelops.com>
2017-03-14 14:28:32 +0100

    NEW: function for dynamically loading content to fields

---
+++ Classes/class.tx_annotation_list.php
@@ -0,0 +1,22 @@
+<?php
+
+require_once(__DIR__."/Domain/Model/SemantifyItWrapper.php");
+
+
+class tx_annotation_list {
+
+    /**
+     * @param $config
+     * @param $annotations
+     * @return mixed
+     */
+    function getList($config, $annotations){
+
+        $Semantify = new SemantifyItWrapper();
+        $annotations = $Semantify->getDomainAnnotations();
+
+        // return config
+        $config['items'] = array_merge($config['items'], $annotations);
+        return $config;
+    }
+}
