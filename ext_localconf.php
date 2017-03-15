0fbdeb8440d31010afb6a3614c07dc6e01dbdc07
Richard Dvorsky <generator@mydevelops.com>
2017-03-15 15:34:26 +0100

    NEW: hooks added for injection

--- ext_localconf.php
+++ ext_localconf.php
@@ -4,3 +4,10 @@ if (!defined ('TYPO3_MODE')) die ('Access denied.');
 $TYPO3_CONF_VARS['FE']['pageOverlayFields'] .= ',semantify_plugin_typo3_annotationID';
 
 //$GLOBALS['TSFE']->set_no_cache();
+
+    // HOOK is called after caching
+    $TYPO3_CONF_VARS['SC_OPTIONS']['tslib/class.tslib_fe.php']['contentPostProc-output'][] = 'EXT:' . $_EXTKEY . '/Classes/class.tx_annotation_input.php:&tx_annotation_input->performNotCached';
+
+    // HOOK is called before caching
+    $TYPO3_CONF_VARS['SC_OPTIONS']['tslib/class.tslib_fe.php']['contentPostProc-all'][] = 'EXT:'. $_EXTKEY .'/Classes/class.tx_annotation_input.php:&tx_annotation_input->performCached';
+
