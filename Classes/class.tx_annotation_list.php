1d0e2bdcdeaae1d5364150a23397bf9e18262d19
Richard Dvorsky <generator@mydevelops.com>
2017-03-15 15:33:41 +0100

    UPDATE: small changes

--- Classes/class.tx_annotation_list.php
+++ Classes/class.tx_annotation_list.php
@@ -3,20 +3,20 @@
 require_once(__DIR__."/Domain/Model/SemantifyItWrapper.php");
 
 
-class tx_annotation_list {
+    class tx_annotation_list {
 
-    /**
-     * @param $config
-     * @param $annotations
-     * @return mixed
-     */
-    function getList($config, $annotations){
+        /**
+         * @param $config
+         * @param $annotations
+         * @return mixed
+         */
+        function getList($config, $annotations){
 
-        $Semantify = new SemantifyItWrapper();
-        $annotations = $Semantify->getDomainAnnotations();
+            $Semantify = new SemantifyItWrapper();
+            $annotations = $Semantify->getDomainAnnotations();
 
-        // return config
-        $config['items'] = array_merge($config['items'], $annotations);
-        return $config;
+            // return config
+            $config['items'] = array_merge($config['items'], $annotations);
+            return $config;
+        }
     }
-}
