ebefe7159acad235da691821ee92236f12c45e7d
Richard Dvorsky <generator@mydevelops.com>
2017-03-28 09:42:09 +0200

    NEW: hook for the saving

--- ext_localconf.php
+++ ext_localconf.php
@@ -11,3 +11,5 @@ $TYPO3_CONF_VARS['FE']['pageOverlayFields'] .= ',semantify_it_annotationID';
     // HOOK is called before caching
     $TYPO3_CONF_VARS['SC_OPTIONS']['tslib/class.tslib_fe.php']['contentPostProc-all'][] = 'EXT:'. $_EXTKEY .'/Classes/class.tx_annotation_input.php:&tx_annotation_input->performCached';
 
+    //hook is called after change of page settings
+    $TYPO3_CONF_VARS['SC_OPTIONS']['t3lib/class.t3lib_tcemain.php']['proc‌​essDatamapClass'][] = 'EXT:'.$_EXTKEY.'/Classes/class.tx_annotation_new.php:&tx_annotation_new->save';
