3e13413e4169aecd5e3dd48ab367a44dd76b2bdd
Richard Dvorsky <generator@mydevelops.com>
2017-03-28 09:41:09 +0200

    new function for testing of the hook

--- Classes/class.tx_annotation_new.php
+++ Classes/class.tx_annotation_new.php
@@ -3,15 +3,37 @@
 require_once(__DIR__."/Domain/Model/SemantifyItWrapper.php");
 
 
-    class tx_annotation_new {
+/**
+ * Class tx_annotation_new
+ */
+class tx_annotation_new {
 
         /**
          * @param $config
          * @param $annotations
          * @return mixed
          */
-        function step_one($config){
+        function step_one($config)
+        {
             $annotations = array('qwddas','d');
             $config['items'] = array_merge($config['items'], $annotations);
         }
+
+        /**
+         *
+         */
+        function save($status, $table, $id, &$fieldArray, &$pObj)
+        {
+            //if($table == 'pages')
+            //{
+                var_dump($id);
+                var_dump($table);
+                var_dump($status);
+                var_dump($fieldArray);
+                var_dump($pObj);
+            //}
+        }
+
+
+
     }
