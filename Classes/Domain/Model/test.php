027a449c936b5145e629c5f450ff8cb75b055ed3
Richard Dvorsky <generator@mydevelops.com>
2017-03-28 11:41:59 +0200

    UPDATE: updated test

--- Classes/Domain/Model/test.php
+++ Classes/Domain/Model/test.php
@@ -1,9 +1,12 @@
 <?php
 
+use \STI\SemantifyIt\Domain\Model\SemantifyItWrapper;
 
 include_once "SemantifyItWrapper.php";
 
+
 $sem = new SemantifyItWrapper("rkvpGNrix");
+
 
 //echo $sem->getAnnotation("rJL4cNBsg");
 echo "<pre>";
