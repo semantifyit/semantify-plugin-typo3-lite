e385d81947183ab15836cb9da6428f6be9a109ac
Richard Dvorsky <generator@mydevelops.com>
2017-03-28 16:10:08 +0200

    NEW: added annotation list for step one and two

--- Configuration/TCA/Overrides/Snippets/fields_annotationNew.php
+++ Configuration/TCA/Overrides/Snippets/fields_annotationNew.php
@@ -1,14 +1,33 @@
 <?php
 
 $fields[] = array(
-    'stepone' => array(
-        'label' => 'LLL:EXT:semantify_it/Resources/Private/Language/locallang_db.xlf:pages.semantify_plugin_typo3_annotationNew',
+    'semantify_it_annotationNew_StepOne' => array(
+        'label' => 'LLL:EXT:semantify_it/Resources/Private/Language/locallang_db.xlf:pages.semantify_it_annotationNew_StepOne',
         'exclude' => 1,
         'config' => array(
             'type' => 'select',
-            'itemsProcFunc' => 'tx_annotation_new->step_one',
             'items' => array(
+                array("LLL:EXT:semantify_it/Resources/Private/Language/locallang_db.xlf:pages.semantify_it_annotationNew_StepOneChoose", "");
+                array('Article', 'article'),
+                array('Blog Posting', 'blogPosting'),
             ),
         ),
     )
 );
+
+$fields[] = array(
+    'semantify_it_annotationNew_StepTwo' => array(
+        'label' => 'LLL:EXT:semantify_it/Resources/Private/Language/locallang_db.xlf:pages.semantify_it_annotationNew_StepTwo',
+        'exclude' => 1,
+        'config' => array(
+            'type' => 'select',
+            'items' => array(
+                array("LLL:EXT:semantify_it/Resources/Private/Language/locallang_db.xlf:pages.semantify_it_annotationNew_StepTwoChoose", "");
+                array('Event', 'article'),
+                array('Place', 'place'),
+                array('Thing', 'thing'),
+            ),
+        ),
+    )
+);
+
