5b12cd1c44e6245147aa600211290f967a11d080
Richard Dvorsky <generator@mydevelops.com>
2017-03-28 16:25:25 +0200

    NEW: added fields for the name and url

--- Configuration/TCA/Overrides/Snippets/fields_annotationNew.php
+++ Configuration/TCA/Overrides/Snippets/fields_annotationNew.php
@@ -2,13 +2,16 @@
 
 $fields[] = array(
     'semantify_it_annotationNew_StepOne' => array(
-        'label' => 'LLL:EXT:semantify_it/Resources/Private/Language/locallang_db.xlf:pages.semantify_it_annotationNew_StepOne',
+        'label'   => 'LLL:EXT:semantify_it/Resources/Private/Language/locallang_db.xlf:pages.semantify_it_annotationNew_StepOne',
         'exclude' => 1,
-        'config' => array(
-            'type' => 'select',
+        'config'  => array(
+            'type'  => 'select',
             'items' => array(
-                array("LLL:EXT:semantify_it/Resources/Private/Language/locallang_db.xlf:pages.semantify_it_annotationNew_StepOneChoose", "");
-                array('Article', 'article'),
+                array(
+                    "LLL:EXT:semantify_it/Resources/Private/Language/locallang_db.xlf:pages.semantify_it_annotationNew_StepOneChoose",
+                    ""
+                );
+array('Article', 'article'),
                 array('Blog Posting', 'blogPosting'),
             ),
         ),
@@ -17,12 +20,15 @@ $fields[] = array(
 
 $fields[] = array(
     'semantify_it_annotationNew_StepTwo' => array(
-        'label' => 'LLL:EXT:semantify_it/Resources/Private/Language/locallang_db.xlf:pages.semantify_it_annotationNew_StepTwo',
+        'label'   => 'LLL:EXT:semantify_it/Resources/Private/Language/locallang_db.xlf:pages.semantify_it_annotationNew_StepTwo',
         'exclude' => 1,
-        'config' => array(
-            'type' => 'select',
+        'config'  => array(
+            'type'  => 'select',
             'items' => array(
-                array("LLL:EXT:semantify_it/Resources/Private/Language/locallang_db.xlf:pages.semantify_it_annotationNew_StepTwoChoose", "");
+                array(
+                    "LLL:EXT:semantify_it/Resources/Private/Language/locallang_db.xlf:pages.semantify_it_annotationNew_StepTwoChoose",
+                    ""
+                );
                 array('Event', 'article'),
                 array('Place', 'place'),
                 array('Thing', 'thing'),
@@ -31,3 +37,36 @@ $fields[] = array(
     )
 );
 
+
+
+
+$fields[] = array(
+    'semantify_it_annotationNew_Name' => array(
+        'l10n_mode' => 'mergeIfNotBlank',
+        'exclude'   => 1,
+        'label'   => 'LLL:EXT:semantify_it/Resources/Private/Language/locallang_db.xlf:pages.semantify_it_annotationNew_Name',
+        'config'    => array(
+            'type'        => 'input',
+            'size'        => '20',
+            'eval'        => 'trim',
+            'placeholder' => '__row|LLL:EXT:semantify_it/Resources/Private/Language/locallang_db.xlf:pages.semantify_it_annotationNew_NameChoose',
+            'mode'        => 'useOrOverridePlaceholder',
+        )
+    ),
+);
+
+
+$fields[] = array(
+    'semantify_it_annotationNew_URL' => array(
+        'l10n_mode' => 'mergeIfNotBlank',
+        'exclude'   => 1,
+        'label'   => 'LLL:EXT:semantify_it/Resources/Private/Language/locallang_db.xlf:pages.semantify_it_annotationNew_URL',
+        'config'    => array(
+            'type'        => 'input',
+            'size'        => '20',
+            'eval'        => 'trim',
+            'placeholder' => '__row|LLL:EXT:semantify_it/Resources/Private/Language/locallang_db.xlf:pages.semantify_it_annotationNew_URLChoose',
+            'mode'        => 'useOrOverridePlaceholder',
+        )
+    ),
+);
