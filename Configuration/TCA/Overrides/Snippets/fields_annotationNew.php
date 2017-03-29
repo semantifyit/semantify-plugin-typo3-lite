1c9b007105b69f67ed1b49cdff817e0438c91141
Richard Dvorsky <generator@mydevelops.com>
2017-03-29 14:06:07 +0200

    NEW: hidding the element

--- Configuration/TCA/Overrides/Snippets/fields_annotationNew.php
+++ Configuration/TCA/Overrides/Snippets/fields_annotationNew.php
@@ -10,6 +10,7 @@ $fields[] = array(
     'semantify_it_annotationNew_StepOne' => array(
         'label'   => 'LLL:EXT:semantify_it/Resources/Private/Language/locallang_db.xlf:pages.semantify_it_annotationNew_StepOne',
         'exclude' => 1,
+        'displayCond' => 'FIELD:semantify_it_annotationID:=:1',
         'config'  => array(
             'type'  => 'select',
             'items' => array(
@@ -20,6 +21,7 @@ $fields[] = array(
                 array('Article', 'article'),
                 array('Blog Posting', 'blogPosting'),
             ),
+
         ),
     )
 );
@@ -50,10 +52,10 @@ $fields[] = array(
         'exclude'   => 1,
         'label'     => 'LLL:EXT:semantify_it/Resources/Private/Language/locallang_db.xlf:pages.semantify_it_annotationNew_Name',
         'config'    => array(
-            'type'        => 'input',
-            'size'        => '255',
-            'eval'        => 'trim',
-            )
+            'type' => 'input',
+            'size' => '255',
+            'eval' => 'trim',
+        )
     ),
 );
 
@@ -63,22 +65,22 @@ $fields[] = array(
         'l10n_mode' => 'mergeIfNotBlank',
         'exclude'   => 1,
         'label'     => 'LLL:EXT:semantify_it/Resources/Private/Language/locallang_db.xlf:pages.semantify_it_annotationNew_URL',
-        'config' => array(
-            'type' => 'input',
-            'size' => '255',
+        'config'    => array(
+            'type'    => 'input',
+            'size'    => '255',
             'softref' => 'typolink',
             'wizards' => array(
                 '_PADDING' => 2,
-                'link' => array(
-                    'type' => 'popup',
-                    'title' => 'Link',
-                    'icon' => 'EXT:example/Resources/Public/Images/FormFieldWizard/wizard_link.gif',
-                    'module' => array(
-                        'name' => 'wizard_element_browser',
+                'link'     => array(
+                    'type'         => 'popup',
+                    'title'        => 'Link',
+                    'icon'         => 'EXT:example/Resources/Public/Images/FormFieldWizard/wizard_link.gif',
+                    'module'       => array(
+                        'name'          => 'wizard_element_browser',
                         'urlParameters' => array(
                             'mode' => 'wizard'
-                        ) ,
-                    ) ,
+                        ),
+                    ),
                     'JSopenParams' => 'height=600,width=500,status=0,menubar=0,scrollbars=1'
                 )
             )
