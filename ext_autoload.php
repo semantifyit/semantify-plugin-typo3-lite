52e28fd20be9f8faef95fb10a1aae77e9e6ff846
Richard Dvorsky <generator@mydevelops.com>
2017-03-30 22:52:35 +0200

    NEW: loading new classes

--- ext_autoload.php
+++ ext_autoload.php
@@ -5,6 +5,7 @@ $extensionClassesPath = \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extP
 $default = array(
     'SemantifyItWrapper' => $extensionClassesPath . 'Domain/Model/SemantifyItWrapper.php',
     'SemantifyItWrapperController' => $extensionClassesPath . 'Controller/SemantifyItWrapperController.php',
+    'SemantifyItController' => $extensionClassesPath . 'Controller/SemantifyItController.php',
     'SemantifyIt' => $extensionClassesPath . 'Vendor/semantify-api-php/SemantifyIt.php',
 
 );
