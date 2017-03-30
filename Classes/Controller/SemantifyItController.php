c5f10f675621efca87edbaff86993a921d5bd7da
Richard Dvorsky <generator@mydevelops.com>
2017-03-30 22:51:40 +0200

    NEW: new functions for handling the creation of the new annotation

--- Classes/Controller/SemantifyItController.php
+++ Classes/Controller/SemantifyItController.php
@@ -1,35 +1,55 @@
 <?php
+
 namespace STI\SemantifyIt\Controller;
 
-use \TYPO3\CMS\Extbase\Mvc\Controller\ActionController;
 use \TYPO3\CMS\Core\Utility\DebugUtility;
 
 
 /**
- * SchemantifyItController
+ * SemantifyItController
  */
-class SchemantifyItController extends ActionController
+class SemantifyItController extends \TYPO3\CMS\Extbase\Mvc\Controller\ActionController
 {
 
-    /**
-     * action main
-     * @return void
-     */
-    public function mainAction()
+    public function listAnnotations()
     {
-        $Semantify = new SemantifyItWrapper();
-        $annotations = $Semantify->getDomainAnnotations();
-        $this->view->assign('annotation_list', $annotations);
-    }
-
-
-
-
-
-    public function listAnnotations() {
         DebugUtility::debug('frontendAction reached', ':)');
         // $GLOBALS['TSFE'] could be accessed here
         $this->view->render();
+    }
+
+
+    private function constructAnnotation($data)
+    {
+        var_dump($data);
+    }
+
+
+    public function createAnnotation($fields, $other)
+    {
+
+       // $this->getControllerContext();
+
+        $data = array();
+        $data['id'] = $other['id'];
+       //not yet working url
+        // $data["url"] = $this->getControllerContext()->getUriBuilder()->reset()->setTargetPageUid($data['id'])->buildFrontendUri();
+        $data['title'] = $fields['title'];
+        $data['nav_title'] = $fields['nav_title'];
+        $data['subtitle'] = $fields['subtitle'];
+        $data['tstamp'] = $other['tstamp'];
+
+
+        //we will choose only the necesarry fields for our semantifyit
+        foreach ($fields as $key => $field) {
+            if (strpos($key, 'semantify_it') !== false) {
+                $data[$key] = $field;
+            }
+        }
+
+        $jsonld = $this->constructAnnotation($data);
+
+
     }
 
 
