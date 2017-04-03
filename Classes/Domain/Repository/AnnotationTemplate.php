0b3cbfc3e60573805c16cca3ae9e2141dc7e44ec
Richard Dvorsky <generator@mydevelops.com>
2017-04-03 11:31:24 +0200

    NEW: class for the annotations

---
+++ Classes/Domain/Repository/AnnotationTemplate.php
@@ -0,0 +1,26 @@
+<?php
+namespace STI\SemantifyIt\Domain\Repository;
+
+
+
+
+/**
+ * Class SemantifyItWrapper
+ */
+abstract class AnnotationTemplate
+{
+
+    /**
+     * SemantifyItWrapper constructor.
+     *
+     * @param string $key
+     */
+    public function __construct($key = "")
+    {
+
+    }
+
+
+
+
+}
