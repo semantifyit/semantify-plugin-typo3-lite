cadc1d799998b11fb4dd0634d783280ce05c51e6
Richard Dvorsky <generator@mydevelops.com>
2017-04-03 13:30:15 +0200

    NEW: class for annotations

--- Classes/Domain/Repository/AnnotationTemplate.php
+++ Classes/Domain/Repository/AnnotationTemplate.php
@@ -1,7 +1,6 @@
 <?php
+
 namespace STI\SemantifyIt\Domain\Repository;
-
-
 
 
 /**
@@ -10,17 +9,42 @@ namespace STI\SemantifyIt\Domain\Repository;
 abstract class AnnotationTemplate
 {
 
-    /**
-     * SemantifyItWrapper constructor.
-     *
-     * @param string $key
-     */
-    public function __construct($key = "")
+    abstract function getAnnotation($fields){}
+
+    static function createAnnotation($fields, $special)
     {
+        $jsonld = '{
+            "@context": "http://schema.org",    
+            "@type": "' . @$fields['@type'] . '",
+            "headline": "' . @$fields['headline'] . '",
+            "datePublished": "2015-02-05T08:00:00+08:00",
+            "dateModified": "2015-02-05T09:20:00+08:00",
+            "description": "' . @$fields['headline'] . '",
+            ' . $special . '
+        }';
+
+        return $jsonld;
 
     }
 
 
+}
 
+class Article extends AnnotationTemplate
+{
+
+
+    static function getAnnotation($fields)
+    {
+
+        $special = '';
+
+        $jsonld = parent::createAnnotation($fields, $special);
+
+        return $jsonld;
+
+    }
 
 }
+
+
