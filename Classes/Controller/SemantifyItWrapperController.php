cfc83f52c810106ceb68914a121bd9246ab78308
Richard Dvorsky <generator@mydevelops.com>
2017-04-04 17:43:22 +0200

    ADD and RENAME:  added classes from SemantifyItController and renamed sem to model

--- Classes/Controller/SemantifyItWrapperController.php
+++ Classes/Controller/SemantifyItWrapperController.php
@@ -4,6 +4,7 @@ namespace STI\SemantifyIt\Controller;
 
 use \TYPO3\CMS\Extbase\Mvc\Controller\ActionController;
 use \TYPO3\CMS\Core\Utility\DebugUtility;
+use \TYPO3\CMS\Core\Utility\GeneralUtility;
 use \STI\SemantifyIt\Domain\Model\SemantifyItWrapper;
 
 /**
@@ -12,7 +13,8 @@ use \STI\SemantifyIt\Domain\Model\SemantifyItWrapper;
 class SemantifyItWrapperController extends ActionController
 {
 
-    private $Sem;
+    public $model;
+
 
     /**
      * displaying warnings
@@ -24,7 +26,24 @@ class SemantifyItWrapperController extends ActionController
 
     function __construct()
     {
-        $this->Sem = new SemantifyItWrapper();
+        $this->model = new SemantifyItWrapper();
+        $this->initialize();
+    }
+
+    /**
+     * @return \STI\SemantifyIt\Domain\Model\SemantifyItWrapper
+     */
+    public function getModel()
+    {
+        return $this->model;
+    }
+
+    /**
+     * @param \STI\SemantifyIt\Domain\Model\SemantifyItWrapper $model
+     */
+    public function setModel($model)
+    {
+        $this->model = $model;
     }
 
 
@@ -122,7 +141,7 @@ class SemantifyItWrapperController extends ActionController
             "1"
         );
 
-        $json = $this->Sem->getAnnotationList();
+        $json = $this->model->getAnnotationList();
 
         //if no data received
         if (!$json) {
@@ -168,6 +187,134 @@ class SemantifyItWrapperController extends ActionController
 
         return $annotationList;
     }
+
+    /**
+     *
+     * function which construct an annotation
+     *
+     * @param $data
+     */
+    private function constructAnnotation($data)
+    {
+        //class choosen by type
+        $class = '\\STI\\SemantifyIt\\Domain\\Repository\\'.$data['@type'];
+        $method = 'getAnnotation';
+        //call the class method
+        return call_user_func_array(array($class, $method), array($data));
+    }
+
+
+    /**
+     *
+     * function which is called to create and handle anotations
+     *
+     *
+     * @param $fields
+     * @param $other
+     */
+    public function createAnnotation($fields, $other)
+    {
+
+        $data = $this->createData($fields, $other);
+        $jsonld = $this->constructAnnotation($data);
+
+        return $jsonld;
+    }
+
+
+    /**
+     *
+     * function which makes a mapping to data array which is after that send to construct annotation
+     *
+     * @param $fields
+     * @param $other
+     * @return array
+     */
+    private function createData($fields, $other){
+        $data = array();
+        $data['dateModified'] = $other['dateModified'];
+        $data['dateCreated'] = $other['dateCreated'];
+        $data['@type'] = $fields['semantify_it_annotationNew_StepOne'];
+        $data['@about'] = $fields['semantify_it_annotationNew_StepTwo'];
+        $data['@aboutName'] = $fields['semantify_it_annotationNew_Name'];
+        $data['@aboutURL'] = $fields['semantify_it_annotationNew_URL'];
+        $data['id'] = $other['id'];
+        $data["url"] = $this->createURLfromID($data['id']);
+        $data['headline'] = $fields['title'];
+        $data['nav_title'] = $fields['nav_title'];
+        $data['subtitle'] = $fields['subtitle'];
+        $data['tstamp'] = $other['tstamp'];
+        $data['name'] = $other['name'];
+        $data['email'] = $other['email'];
+        return $data;
+    }
+
+
+    /**
+     *
+     * function which take a care of getting url from the string
+     *
+     * @param $id
+     * @return string
+     */
+    private function createURLfromID($id)
+    {
+        $fullURL = GeneralUtility::getIndpEnv('TYPO3_SITE_URL');
+        $cObject = GeneralUtility::makeInstance('TYPO3\CMS\Frontend\ContentObject\ContentObjectRenderer');
+        $configurations['returnLast'] = 'url'; // get it as URL
+        $configurations['parameter'] = $id;
+        $url = $fullURL . htmlspecialchars($cObject->typolink(null, $configurations));
+
+        return $url;
+    }
+
+    /**
+     *
+     * function which initializes global variables of typo3
+     *
+     */
+    private function initialize()
+    {
+        if (!isset($GLOBALS['TSFE'])) {
+
+            $pid = (int)GeneralUtility::_POST('pid');
+            $rootline =
+                \TYPO3\CMS\Backend\Utility\BackendUtility::BEgetRootLine($pid);
+
+            foreach ($rootline as $page) {
+                if ($page['is_siteroot']) {
+                    $id = (int)$page['uid'];
+                    break;
+                }
+            }
+
+            $type = 0;
+
+            if (!is_object($GLOBALS['TT'])) {
+                $GLOBALS['TT'] = new \TYPO3\CMS\Core\TimeTracker\NullTimeTracker;
+                $GLOBALS['TT']->start();
+            }
+
+            $GLOBALS['TSFE'] =
+                GeneralUtility::makeInstance('TYPO3\CMS\Frontend\Controller\TypoScriptFrontendController',
+                                             $GLOBALS['TYPO3_CONF_VARS'], $id, $type);
+            $GLOBALS['TSFE']->connectToDB();
+            $GLOBALS['TSFE']->initFEuser();
+            $GLOBALS['TSFE']->determineId();
+            $GLOBALS['TSFE']->initTemplate();
+            $GLOBALS['TSFE']->getConfigArray();
+
+            if
+            (\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::isLoaded('realurl')
+            ) {
+                $host =
+                    \TYPO3\CMS\Backend\Utility\BackendUtility::firstDomainRecord($rootline);
+                $_SERVER['HTTP_HOST'] = $host;
+            }
+        }
+
+    }
+
 
 
 }
