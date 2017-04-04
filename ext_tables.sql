4db8750f68d9f687c2d1b2cc57d19304d2776b54
Richard Dvorsky <generator@mydevelops.com>
2017-04-04 22:54:36 +0200

    UPDATE: updated length

--- ext_tables.sql
+++ ext_tables.sql
@@ -4,11 +4,11 @@
 
 CREATE TABLE pages (
 
-  semantify_it_annotationID varchar(255) DEFAULT '' NOT NULL
-  semantify_it_annotationNew_ID varchar(255) DEFAULT '' NOT NULL
-  semantify_it_annotationNew_StepOne varchar(255) DEFAULT '' NOT NULL
-  semantify_it_annotationNew_StepTwo varchar(255) DEFAULT '' NOT NULL
-  semantify_it_annotationNew_Name varchar(255) DEFAULT '' NOT NULL
+  semantify_it_annotationID varchar(10) DEFAULT '' NOT NULL,
+  semantify_it_annotationNew_ID varchar(10) DEFAULT '' NOT NULL,
+  semantify_it_annotationNew_StepOne varchar(255) DEFAULT '' NOT NULL,
+  semantify_it_annotationNew_StepTwo varchar(255) DEFAULT '' NOT NULL,
+  semantify_it_annotationNew_Name varchar(255) DEFAULT '' NOT NULL,
   semantify_it_annotationNew_URL varchar(255) DEFAULT '' NOT NULL
 
 );
@@ -17,10 +17,10 @@ CREATE TABLE pages (
 # Modifying pages_language_overlay table
 #
 CREATE TABLE pages_language_overlay (
-  semantify_it_annotationID varchar(255) DEFAULT '' NOT NULL
-  semantify_it_annotationNew_ID varchar(255) DEFAULT '' NOT NULL
-  semantify_it_annotationNew_StepOne varchar(255) DEFAULT '' NOT NULL
-  semantify_it_annotationNew_StepTwo varchar(255) DEFAULT '' NOT NULL
-  semantify_it_annotationNew_Name varchar(255) DEFAULT '' NOT NULL
+  semantify_it_annotationID varchar(10) DEFAULT '' NOT NULL,
+  semantify_it_annotationNew_ID varchar(10) DEFAULT '' NOT NULL,
+  semantify_it_annotationNew_StepOne varchar(255) DEFAULT '' NOT NULL,
+  semantify_it_annotationNew_StepTwo varchar(255) DEFAULT '' NOT NULL,
+  semantify_it_annotationNew_Name varchar(255) DEFAULT '' NOT NULL,
   semantify_it_annotationNew_URL varchar(255) DEFAULT '' NOT NULL
 );
