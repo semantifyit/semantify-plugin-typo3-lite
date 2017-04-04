1e842fe53a4da6ca2a0587c54b0552b5a8b5cf50
Richard Dvorsky <generator@mydevelops.com>
2017-04-04 22:54:57 +0200

    REMOVE: removed parts which are not necesary

--- ext_tables.php
+++ ext_tables.php
@@ -8,42 +8,9 @@
 
 if (!defined ('TYPO3_MODE')) die ('Access denied.');
 
-/*
-\TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerPlugin(
-    'STI.' . $_EXTKEY,
-    'SemantifyIt',
-    'SemantifyIt'
-);
-*/
-/*
-if (TYPO3_MODE === 'BE') {
-
-
-      // Registers a Backend Module
-
-    \TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerModule(
-        'STI.' . $_EXTKEY,
-        'web',	 // Make module a submodule of 'web'
-        'SemantifyIt',	// Submodule key
-        '',						// Position
-        array(
-            'SemantifyIt' => 'main, saveForm, listPages',
-        ),
-        array(
-            'access' => 'user,group',
-            'icon'   => 'EXT:' . $_EXTKEY . '/ext_icon.png',
-            'labels' => 'LLL:EXT:' . $_EXTKEY . '/Resources/Private/Language/locallang_db.xlf',
-        )
-    );
-
-}
-*/
-
-
 
 include_once(\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extPath($_EXTKEY).'Classes/class.tx_annotation_list.php');
 include_once(\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extPath($_EXTKEY).'Classes/Controller/SemantifyItWrapperController.php');
-include_once(\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extPath($_EXTKEY).'Classes/Controller/SemantifyItController.php');
 include_once(\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extPath($_EXTKEY).'Classes/Domain/Model/SemantifyItWrapper.php');
 include_once(\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extPath($_EXTKEY).'Classes/Domain/Repository/AnnotationTemplate.php');
 include_once(\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extPath($_EXTKEY).'Classes/Vendor/semantify-api-php/SemantifyIt.php');
