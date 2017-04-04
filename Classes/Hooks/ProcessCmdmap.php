f614c40cc75ef417745341900d40efc821395737
Richard Dvorsky <generator@mydevelops.com>
2017-04-04 17:45:01 +0200

    UPDATE: changed to wrapper controller NEW: new debug function and mapping fucntion NEW: function which sends annotations to server

--- Classes/Hooks/ProcessCmdmap.php
+++ Classes/Hooks/ProcessCmdmap.php
@@ -1,20 +1,23 @@
 <?php
-
 namespace STI\SemantifyIt\Hooks;
 
-use \STI\SemantifyIt\Controller\SemantifyItController;
+use STI\SemantifyIt\Controller\SemantifyItWrapperController;
 
 class ProcessCmdmap
 {
-
-
+    //unused hooks
     //public function processCmdmap_preProcess($command, $table, $id, $value, \TYPO3\CMS\Core\DataHandling\DataHandler &$pObj) { }
-
     //public function processDatamap_preProcessFieldArray(array &$fieldArray, $table, $id, \TYPO3\CMS\Core\DataHandling\DataHandler &$pObj) {}
-
     //public function processDatamap_afterDatabaseOperations($status, $table, $id, array $fieldArray, \TYPO3\CMS\Core\DataHandling\DataHandler &$pObj) {echo 7;}
 
 
+    /**
+     * @param                                          $status
+     * @param                                          $table
+     * @param                                          $id
+     * @param array                                    $fieldArray
+     * @param \TYPO3\CMS\Core\DataHandling\DataHandler $pObj
+     */
     public function processDatamap_postProcessFieldArray(
         $status,
         $table,
@@ -23,32 +26,64 @@ class ProcessCmdmap
         \TYPO3\CMS\Core\DataHandling\DataHandler &$pObj
     ) {
 
-        /*
-        echo "**************ID**************"."<br>";
-        var_dump($id);
-        echo "**************TABLE**************"."<br>";
-        var_dump($table);
-        echo "**************STATUS**************"."<br>";
-        var_dump($status);
-        echo "**************FIELDARRAY**************"."<br>";
-        var_dump($fieldArray);
-        echo "**************POBJ**************"."<br>";
-        //var_dump($pObj);
-        */
-        //var_dump($pObj->BE_USER->user['realName']);
+        //$this->hookDebug($status, $table, $id, $fieldArray, $pObj);
 
-        if($table=="pages"){
-            $other = array();
-            $other['id'] = $id;
-            $other['dateModified'] = $fieldArray['tstamp'];
-            $other['dateCreated'] = $pObj->checkValue_currentRecord['crdate'];
-            $other['name']        = $pObj->BE_USER->user['realName'];
-            $other['email']       = $pObj->BE_USER->user['email'];
+        //value of stepone
+        $stepone = $pObj->datamap['pages'][$id]["semantify_it_annotationNew_StepOne"];
+        //value of the new annotation
+        $annotationID = $pObj->datamap['pages'][$id]["semantify_it_annotationID"];
 
-            $semantify = new SemantifyItController();
-            $semantify->createAnnotation($pObj->datamap['pages'][$id], $other);
+        //we make a new annotation if we have table pages and field stepone is not empty
+        if(($table=="pages") && ($stepone!="")){
+
+            $other=$this->dataMapping($id, $fieldArray, $pObj);
+
+            $semantify = new SemantifyItWrapperController();
+            $newAnnotation =  $semantify->createAnnotation($pObj->datamap['pages'][$id], $other);
+
+
+            //if it is a new annotation
+            if($annotationID=="1"){
+                $response =  $semantify->model->postAnnotation($newAnnotation);
+                var_dump($response);
+
+            }
+
         }
 
     }
+
+    /**
+     * @param $id
+     * @param $fieldArray
+     * @param $pObj
+     * @return array
+     */
+    private function dataMapping(&$id, &$fieldArray, &$pObj){
+        $other = array();
+        $other['id'] = $id;
+        $other['dateModified'] = $fieldArray['tstamp'];
+        $other['dateCreated'] = $pObj->checkValue_currentRecord['crdate'];
+        $other['name']        = $pObj->BE_USER->user['realName'];
+        $other['email']       = $pObj->BE_USER->user['email'];
+
+        return $other;
+    }
+
+    private function hookDebug(&$status, &$table, &$id, &$fieldArray, &$pObj){
+
+      echo "**************ID**************"."<br>";
+      var_dump($id);
+      echo "**************TABLE**************"."<br>";
+      var_dump($table);
+      echo "**************STATUS**************"."<br>";
+      var_dump($status);
+      echo "**************FIELDARRAY**************"."<br>";
+      var_dump($fieldArray);
+      echo "**************POBJ**************"."<br>";
+      var_dump($pObj);
+
+    }
+
 
 }
