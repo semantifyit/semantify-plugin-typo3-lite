d461bdccccf548cdfd0fb783dacb31ed4a301a91
Richard Dvorsky <generator@mydevelops.com>
2017-04-04 21:55:37 +0200

    CHANGE: changed hooks to preprocess, because in the case before saving not working

--- Classes/Hooks/ProcessCmdmap.php
+++ Classes/Hooks/ProcessCmdmap.php
@@ -7,7 +7,17 @@ class ProcessCmdmap
 {
     //unused hooks
     //public function processCmdmap_preProcess($command, $table, $id, $value, \TYPO3\CMS\Core\DataHandling\DataHandler &$pObj) { }
-    //public function processDatamap_preProcessFieldArray(array &$fieldArray, $table, $id, \TYPO3\CMS\Core\DataHandling\DataHandler &$pObj) {}
+    public function processDatamap_postProcessFieldArray(
+        $status,
+        $table,
+        $id,
+        array &$fieldArray,
+        \TYPO3\CMS\Core\DataHandling\DataHandler &$pObj
+    ){
+        echo "POST";
+        $this->hookDebug($status, $table, $id, $fieldArray, $pObj);
+
+    }
     //public function processDatamap_afterDatabaseOperations($status, $table, $id, array $fieldArray, \TYPO3\CMS\Core\DataHandling\DataHandler &$pObj) {echo 7;}
 
 
@@ -18,35 +28,39 @@ class ProcessCmdmap
      * @param array                                    $fieldArray
      * @param \TYPO3\CMS\Core\DataHandling\DataHandler $pObj
      */
-    public function processDatamap_postProcessFieldArray(
-        $status,
-        $table,
-        $id,
-        array &$fieldArray,
-        \TYPO3\CMS\Core\DataHandling\DataHandler &$pObj
-    ) {
-
+    public function processDatamap_preProcessFieldArray(array &$fieldArray, $table, $id, \TYPO3\CMS\Core\DataHandling\DataHandler &$pObj) {
+        echo "PRE";
         //$this->hookDebug($status, $table, $id, $fieldArray, $pObj);
 
-        //value of stepone
-        $stepone = $pObj->datamap['pages'][$id]["semantify_it_annotationNew_StepOne"];
-        //value of the new annotation
-        $annotationID = $pObj->datamap['pages'][$id]["semantify_it_annotationID"];
+        ////value of the new annotation
+        $newID = $pObj->datamap['pages'][$id]["semantify_it_annotationNew_ID"];
+        //value of the current annotation
+        $annotationID = $fieldArray["semantify_it_annotationID"];
+
+        $name = $fieldArray["semantify_it_annotationNew_Name"];
 
         //we make a new annotation if we have table pages and field stepone is not empty
-        if(($table=="pages") && ($stepone!="")){
-
+        if(($table=="pages") && ($name!="")){
+            //if it is a new annotation
             $other=$this->dataMapping($id, $fieldArray, $pObj);
 
             $semantify = new SemantifyItWrapperController();
             $newAnnotation =  $semantify->createAnnotation($pObj->datamap['pages'][$id], $other);
 
-
             //if it is a new annotation
             if($annotationID=="1"){
-                $response =  $semantify->model->postAnnotation($newAnnotation);
-                var_dump($response);
+                echo "Post";
+                $id =  $semantify->postAnnotation($newAnnotation);
+                $fieldArray["semantify_it_annotationNew_ID"] = $id;
+                $fieldArray["semantify_it_annotationID"] = $id;
+            }
 
+            //check if there is a new annotation id and it is a same as current annotation choosen one
+            if((isset($newID)) && ($newID!="") && ($newID == $annotationID)){
+                echo "Update";
+                $id =  $semantify->updateAnnotation($updateAnnotation, $newID);
+                $fieldArray["semantify_it_annotationNew_ID"] = $id;
+                $fieldArray["semantify_it_annotationID"] = $id;
             }
 
         }
