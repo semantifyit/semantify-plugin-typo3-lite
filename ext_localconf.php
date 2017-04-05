59d6c20200602869270a966e64a60fe5219fa159
Richard Dvorsky <generator@mydevelops.com>
2017-04-05 09:11:25 +0200

    REMOVE: removed unnecessary parts

--- ext_localconf.php
+++ ext_localconf.php
@@ -19,18 +19,6 @@ $TYPO3_CONF_VARS['FE']['pageOverlayFields'] .= ',semantify_it_annotationNew_URL'
     $TYPO3_CONF_VARS['SC_OPTIONS']['tslib/class.tslib_fe.php']['contentPostProc-all'][] = 'EXT:'. $_EXTKEY .'/Classes/class.tx_annotation_input.php:&tx_annotation_input->performCached';
 
     //hook is called after change of page settings
-/*
-    $TYPO3_CONF_VARS['SC_OPTIONS']['t3lib/class.t3lib_tcemain.php']['proc‌​essDatamapClass'][] = 'EXT:'.$_EXTKEY.'/Classes/class.tx_annotation_new.php:&tx_annotation_new->save';
-    $GLOBALS ['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_tcemain.php']['processDatamapClass'][]='EXT:'.$_EXTKEY.'/Classes/class.tx_annotation_new.php:&tx_annotation_new->save';
-
-    $GLOBALS ['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_tcemain.php']['processDatamapClass']['extkey'] = 'EXT:'.$_EXTKEY.'/Classes/class.tx_annotation_new.php:&tx_annotation_new->save';
-    $GLOBALS ['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_tcemain.php']['processCmdmapClass']['extkey'] = 'EXT:'.$_EXTKEY.'/Classes/class.tx_annotation_new.php:&tx_annotation_new->save';
-    $GLOBALS ['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_tcemain.php']['processCmdmapClass'][$_EXTKEY] = 'EXT:'.$_EXTKEY.'/Classes/class.tx_annotation_new.php:&tx_annotation_new->save';
-
-    $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_tcemain.php']['processCmdmapClass'][] = 'STI\SemantifyIt\Hooks\ProcessCmdmap';
-    $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['cms/tslib/class.tslib_menu.php']['filterMenuPages']['STI\SemantifyIt\Hooks\ProcessCmdmap'] = 'EXT:'.$_EXTKEY.'/Classes/Hooks/ProcessCmdmap.php:STI\SemantifyIt\Hooks\ProcessCmdmap';
-*/
-
 $GLOBALS ['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_tcemain.php']['processDatamapClass'][''] = 'STI\SemantifyIt\Hooks\ProcessCmdmap';
 $GLOBALS ['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_tcemain.php']['processCmdmapClass'][''] = 'STI\SemantifyIt\Hooks\ProcessCmdmap';
 
