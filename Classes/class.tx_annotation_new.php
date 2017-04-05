43951f88e0bee81287620699dc3f4dbf3dd08cfe
Richard Dvorsky <generator@mydevelops.com>
2017-04-05 09:08:35 +0200

    REMOVE: unused class

--- Classes/class.tx_annotation_new.php
+++ Classes/class.tx_annotation_new.php
@@ -7,36 +7,5 @@
  */
 class tx_annotation_new {
 
-        /**
-         * @param $config
-         * @param $annotations
-         * @return mixed
-         */
-       public function step_one($config)
-        {
-            $annotations = array('qwddas','d');
-            $config['items'] = array_merge($config['items'], $annotations);
-        }
-
-        /**
-         *
-         */
-       public function save($status, $table, $id, &$fieldArray, &$pObj)
-        {
-
-            /** @var $logger \TYPO3\CMS\Core\Log\Logger */
-            $logger = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance('TYPO3\CMS\Core\Log\LogManager')->getLogger(__CLASS__);
-            $logger->info('Everything went fine.');
-            //if($table == 'pages')
-            //{
-                var_dump($id);
-                var_dump($table);
-                var_dump($status);
-                var_dump($fieldArray);
-                var_dump($pObj);
-            //}
-        }
-
-
 
     }
