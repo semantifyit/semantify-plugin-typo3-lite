7ee392a8e233f439ee94857e2858b1e7898017b3
Richard Dvorsky <generator@mydevelops.com>
2017-04-06 14:58:53 +0200

    FIX: version dependency

--- ext_emconf.php
+++ ext_emconf.php
@@ -24,7 +24,7 @@ $EM_CONF[$_EXTKEY] = array(
     'version' => '0.1.4',
     'constraints' => array(
         'depends' => array(
-            'typo3' => '6.2',
+            'typo3' => '6.2.0-8.5.99',
         ),
         'conflicts' => array(
         ),
