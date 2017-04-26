15ed1059579944d501be98ea1e4540abf2bb259f
Richard Dvorsky <generator@mydevelops.com>
2017-04-26 14:02:26 +0200

    FIX: fixed naming

--- ext_tables.sql
+++ ext_tables.sql
@@ -4,24 +4,24 @@
 
 CREATE TABLE pages (
 
-  semantify_it_lite_annotationID varchar(10) DEFAULT '' NOT NULL,
-  semantify_it_lite_annotationNew_ID varchar(10) DEFAULT '' NOT NULL,
-  semantify_it_lite_annotationNew_StepOne varchar(255) DEFAULT '' NOT NULL,
-  semantify_it_lite_annotationNew_StepTwo varchar(255) DEFAULT '' NOT NULL,
-  semantify_it_lite_annotationNew_Name varchar(255) DEFAULT '' NOT NULL,
-  semantify_it_lite_annotationNew_URL varchar(255) DEFAULT '' NOT NULL,
-  semantify_it_lite_annotationNew_RAW text NOT NULL
+  mayrhofen_annotator_annotationID varchar(10) DEFAULT '' NOT NULL,
+  mayrhofen_annotator_annotationNew_ID varchar(10) DEFAULT '' NOT NULL,
+  mayrhofen_annotator_annotationNew_StepOne varchar(255) DEFAULT '' NOT NULL,
+  mayrhofen_annotator_annotationNew_StepTwo varchar(255) DEFAULT '' NOT NULL,
+  mayrhofen_annotator_annotationNew_Name varchar(255) DEFAULT '' NOT NULL,
+  mayrhofen_annotator_annotationNew_URL varchar(255) DEFAULT '' NOT NULL,
+  mayrhofen_annotator_annotationNew_RAW text NOT NULL
 );
 
 #
 # Modifying pages_language_overlay table
 #
 CREATE TABLE pages_language_overlay (
-  semantify_it_lite_annotationID varchar(10) DEFAULT '' NOT NULL,
-  semantify_it_lite_annotationNew_ID varchar(10) DEFAULT '' NOT NULL,
-  semantify_it_lite_annotationNew_StepOne varchar(255) DEFAULT '' NOT NULL,
-  semantify_it_lite_annotationNew_StepTwo varchar(255) DEFAULT '' NOT NULL,
-  semantify_it_lite_annotationNew_Name varchar(255) DEFAULT '' NOT NULL,
-  semantify_it_lite_annotationNew_URL varchar(255) DEFAULT '' NOT NULL,
-  semantify_it_lite_annotationNew_RAW text NOT NULL
+  mayrhofen_annotator_annotationID varchar(10) DEFAULT '' NOT NULL,
+  mayrhofen_annotator_annotationNew_ID varchar(10) DEFAULT '' NOT NULL,
+  mayrhofen_annotator_annotationNew_StepOne varchar(255) DEFAULT '' NOT NULL,
+  mayrhofen_annotator_annotationNew_StepTwo varchar(255) DEFAULT '' NOT NULL,
+  mayrhofen_annotator_annotationNew_Name varchar(255) DEFAULT '' NOT NULL,
+  mayrhofen_annotator_annotationNew_URL varchar(255) DEFAULT '' NOT NULL,
+  mayrhofen_annotator_annotationNew_RAW text NOT NULL
 );
