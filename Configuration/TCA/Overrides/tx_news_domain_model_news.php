4fcad2b8d59c98862a5d07d8c5bfa4db3f1611cf
Richard Dvorsky <generator@mydevelops.com>
2017-05-10 18:04:41 +0200

    NEW: support for tx_news_domain_model_news

---
+++ Configuration/TCA/Overrides/tx_news_domain_model_news.php
@@ -0,0 +1,34 @@
+<?php
+if (!defined('TYPO3_MODE')) {
+    die ('Access denied.');
+}
+
+$table = "tx_news_domain_model_news";
+
+// Configure new fields for annotation id
+include_once("Snippets/fields_annotationID.php");
+
+include_once("Snippets/fields_annotationNew.php");
+
+// Add new fields to pages:
+foreach ($fields as $field){
+    \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTCAcolumns($table, $field);
+
+}
+
+// Make fields visible in the TCEforms with title:
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addToAllTCAtypes(
+    $table, // Table name
+    '--palette--;LLL:EXT:mayrhofen_annotator/Resources/Private/Language/locallang_db.xlf:pages.palette_title;mayrhofen_annotator', // Field list to add
+    '1', // List of specific types to add the field list to. (If empty, all type entries are affected)
+    'after:nav_title' // Insert fields before (default) or after one, or replace a field
+);
+
+
+
+// Add the new palette:
+$GLOBALS['TCA'][$table]['palettes']['mayrhofen_annotator'] = array(
+    'showitem' => implode(', --linebreak-- ,',$ids)
+);
+
+$GLOBALS['TCA'][$table]['ctrl']['requestUpdate'] .= ',mayrhofen_annotator_annotationID';
--- ext_tables.sql
+++ ext_tables.sql
@@ -25,3 +25,16 @@ CREATE TABLE pages_language_overlay (
   mayrhofen_annotator_annotationNew_URL varchar(255) DEFAULT '' NOT NULL,
   mayrhofen_annotator_annotationNew_RAW text
 );
+
+#
+# Modifying tx_news_domain_model_news table
+#
+CREATE TABLE tx_news_domain_model_news (
+  mayrhofen_annotator_annotationID varchar(10) DEFAULT '' NOT NULL,
+  mayrhofen_annotator_annotationNew_ID varchar(10) DEFAULT '' NOT NULL,
+  mayrhofen_annotator_annotationNew_StepOne varchar(255) DEFAULT '' NOT NULL,
+  mayrhofen_annotator_annotationNew_StepTwo varchar(255) DEFAULT '' NOT NULL,
+  mayrhofen_annotator_annotationNew_Name varchar(255) DEFAULT '' NOT NULL,
+  mayrhofen_annotator_annotationNew_URL varchar(255) DEFAULT '' NOT NULL,
+  mayrhofen_annotator_annotationNew_RAW text
+);
